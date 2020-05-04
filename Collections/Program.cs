fa6db6572cb56877902c4fd822fca19332ff9d2b
Mehedi Zamadar <meheditcf@gmail.com>
2020-05-04 23:37:26 +0600

    mandatory commit, Collections\

---
+++ Collections/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Arraylist
+            ArrayList myList = new ArrayList();
+            myList.Add(1);
+            myList.Add("Hi");
+            ArrayList myList2 = new ArrayList();
+            myList2.Add("Hello");
+            myList.AddRange(myList2);
+            myList.Insert(1, 4);
+            for(int i=0; i<myList.Count; i++)
+            {
+                Console.WriteLine(myList[i]);
+            }
+        }
+    }
+}
---
+++ Tuple/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Tuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = Tuple.Create(1, "Steve", "Jobs");
+            Console.WriteLine(person.Item1); // returns 1
+            Console.WriteLine(person.Item2); // returns "Steve"
+            Console.WriteLine(person.Item3); // returns "Jobs"
+        }
+    }
+}
